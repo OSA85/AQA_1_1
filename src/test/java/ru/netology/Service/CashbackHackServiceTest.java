0599ae26a62266f3bdbbcac1598ded91a5ca760c
Sergey Obidin <player181285@gmail.com>
2022-01-09 16:29:00 +0400

    added new test in testng

---
+++ src/test/java/ru/netology/Service/CashbackHackServiceTest.java
@@ -0,0 +1,63 @@
+package ru.netology.Service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldCashbackHackServiceIf0() {
+        int amount = 0;
+        int expected = 1000;
+
+        assertEquals(service.remain(amount), expected);
+
+    }
+
+
+    @Test
+    public void shouldCashbackHackServiceBelow1000() {
+        int amount = 500;
+        int expected = 500;
+
+        assertEquals(service.remain(amount), expected);
+
+    }
+    @Test
+    public void shouldCashbackHackService999() {
+        int amount = 999;
+        int expected = 1;
+
+        assertEquals(service.remain(amount), expected);
+
+    }
+    @Test
+    public void shouldCashbackHackServiceBorder1000() {
+        int amount = 1_000;
+        int expected = 0;
+
+        assertEquals(service.remain(amount), expected);
+
+    }
+
+    @Test
+    public void shouldCashbackHackService1001() {
+        int amount = 1_001;
+        int expected = 999;
+
+        assertEquals(service.remain(amount), expected);
+
+    }
+
+    @Test
+    public void shouldCashbackHackServiceMore1000() {
+        int amount = 1_700;
+        int expected = 300;
+
+        assertEquals(service.remain(amount), expected);
+
+    }
+
+}
